c0098b47e0c1f83a24928a43ab061732c90b09c9
justynadan <justyna.danielewicz@lombard.co.uk>
2023-10-02 10:36:16 +0100

    First Commit

---
+++ BanketyBankCryptoWallet/src/test/java/com/example/BanketyBankCryptoWallet/BanketyBankCryptoWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BanketyBankCryptoWallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanketyBankCryptoWalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
